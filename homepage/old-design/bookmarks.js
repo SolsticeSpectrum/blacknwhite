8fa6799baebd2641277e5e201c0f31008ace5cc3
DarkReaper231 <38611351+DarkReaper231@users.noreply.github.com>
2021-08-16 21:15:29 +0200

    Add files via upload

---
+++ homepage/old-design/bookmarks.js
@@ -0,0 +1,44 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "Drive", url: "https://drive.google.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Soundcloud", url: "https://soundcloud.com" },
+    ],
+  },
+  {
+    title: "Reddit",
+    links: [
+      { name: "/r/overwatch", url: "https://reddit.com/r/overwatch" },
+      {
+        name: "/r/pcmasterrace",
+        url: "https://reddit.com/r/pcmasterrace",
+      },
+      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
+      {
+        name: "/r/battlestations",
+        url: "https://reddit.com/r/battlestations",
+      },
+      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
+      { name: "/r/news", url: "https://reddit.com/r/news" },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "Twitter", url: "https://twitter.com" },
+      { name: "Facebook", url: "https://facebook.com" },
+      { name: "Instagram", url: "https://instagram.com" },
+    ],
+  },
+];
